a796940070548a0a93a83793255079ff2fec5bea
green <whssodi@gmail.com>
2024-06-08 13:32:18 +0900

    feat : cafekiosk test

---
+++ src/main/java/simple/cafekiosk/unit/beverage/Beverage.java
@@ -0,0 +1,8 @@
+package simple.cafekiosk.unit.beverage;
+
+public interface Beverage {
+
+    String getName();
+
+    int getPrice();
+}
---
+++ src/test/java/simple/cafekiosk/unit/CafeKioskTest.java
@@ -0,0 +1,49 @@
+package simple.cafekiosk.unit;
+
+import org.junit.jupiter.api.Test;
+import simple.cafekiosk.unit.beverage.Americano;
+import simple.cafekiosk.unit.beverage.Latte;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CafeKioskTest {
+    
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+        assertThat(cafeKiosk.getBeverages().get(0).getPrice()).isEqualTo(4000);
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(0);
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).hasSize(0);
+    }
+
+
+}
