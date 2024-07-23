a3fba3f1ddc550b1ee65c946c20b1409c12e8da3
green <green@mobidoc.us>
2024-07-23 21:53:41 +0900

    feat : business Layer 2 끝

--- src/main/java/simple/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/simple/cafekiosk/spring/api/service/order/OrderService.java
@@ -1,6 +1,5 @@
 package simple.cafekiosk.spring.api.service.order;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import simple.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
@@ -12,6 +11,9 @@ import simple.cafekiosk.spring.domain.order.Order;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class OrderService {
@@ -21,11 +23,21 @@ public class OrderService {
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        List<Product> duplicateProducts = findProductBy(productNumbers);
 
-        Order order = Order.create(products, registeredDateTime);
+        Order order = Order.create(duplicateProducts, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
         return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+                .map(productMap::get)
+                .collect(Collectors.toList());
     }
 
 }
